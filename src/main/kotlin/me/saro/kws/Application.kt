7f95c8a6de01bf3bc127c1e4b4095d685c93432e
PARK Yong Seo <j@saro.me>
2019-04-11 01:08:16 +0900

    깔끔한 어플리케이션

--- src/main/kotlin/me/saro/kws/Application.kt
+++ src/main/kotlin/me/saro/kws/Application.kt
@@ -7,29 +7,14 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class Application : CommandLineRunner {
-	override fun run(vararg args: String?) {
-		//var map = Converter
-		for ((k, v) in Converter.toMap<String, String>("key", "val", "이름", "홍길동")) {
-			println("$k : $v")
-		}
-
-		var list = arrayListOf("가", "나", "다", "A", "B")
-//		// 안됨.
-//		for ((i, v) in list) {
-//			println("[$i] : $v")
-//		}
-		println(list)
-
-		var a = "asdf"
-		var b = if (a.isNotEmpty())  1 else 2
-		// var c = a.length > 0  ? 1 : 2 // 3항식은 안됨...;
-		println("b의 값음 $b")
+	override fun run(vararg args: String) {
+        println("기존 어플리케이션 러너가 실행되는 모습을 볼 수 있습니다.")
+        println("요즘 언어답게 세미콜론이 필요하지 않습니다.")
 	}
-
 }
 
 fun main(args: Array<String>) {
-	print("hi111")
+	println("프로젝트 실행준비")
 	runApplication<Application>(*args)
 }
 

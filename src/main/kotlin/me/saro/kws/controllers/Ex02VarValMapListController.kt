48f4aea1b95874a745fa2e5812ff624164f00c36
PARK Yong Seo <j@saro.me>
2019-04-13 02:11:54 +0900

    변수, 종결자, 맵, 리스트

--- src/main/kotlin/me/saro/kws/controllers/Ex02VarValMapListController.kt
+++ src/main/kotlin/me/saro/kws/controllers/Ex02VarValMapListController.kt
@@ -3,43 +3,48 @@ package me.saro.kws.controllers
 import me.saro.commons.Converter
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import javax.servlet.http.HttpServletRequest
-import javax.websocket.server.PathParam
 
 @Controller()
 @RequestMapping("/ex02")
 class Ex02VarValMapListController {
 
-    /**
-     * 준비
-     */
+    @GetMapping("/1")
+    @ResponseBody
+    fun no1(): String {
+        var a = "변수 입니다."
+        a += "추가 대입"
+        val b = "변경금지 상수입니다."
+        // b += "추가대입" 오류
+        return "$a / $b"
+    }
+
+    @GetMapping("/2")
+    @ResponseBody
+    fun no2(): String {
+        var sb = StringBuilder();
+
+        for ((k, v) in Converter.toMap<String, String>("key", "val", "이름", "홍길동")) {
+            sb.append("$k : $v")
+        }
+
+        return sb.toString();
+    }
+
     @GetMapping("/3")
     @ResponseBody
     fun no3(req: HttpServletRequest): String {
 
-        println("일반적이 함수형태")
+        val sb = StringBuilder();
+        var list = arrayListOf("가", "나", "다", "A", "B")
 
-        //var map = Converter
-        for ((k, v) in Converter.toMap<String, String>("key", "val", "이름", "홍길동")) {
-            println("$k : $v")
+        for (s in list) {
+            sb.append("$s, ")
         }
 
-        var list = arrayListOf("가", "나", "다", "A", "B")
-    //		// 안됨.
-    //		for ((i, v) in list) {
-    //			println("[$i] : $v")
-    //		}
-        println(list)
-
-        var a = "asdf"
-        var b = if (a.isNotEmpty())  1 else 2
-        // var c = a.length > 0  ? 1 : 2 // 3항식은 안됨...;
-        println("b의 값음 $b")
-
-        return "111";
+        return sb.toString();
     }
 
 }
